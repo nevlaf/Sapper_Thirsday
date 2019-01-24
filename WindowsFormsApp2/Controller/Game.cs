ab457a2f5db405d0367dbb785b35e885de5c4dcd
enevlf@gmail.com <enevlf@gmail.com>
2019-01-24 21:57:49 +0400

    Preliminary

---
+++ WindowsFormsApp2/Controller/Game.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WindowsFormsApp2.Model;
+
+namespace WindowsFormsApp2.Controller
+{
+    public static class Game
+    {
+        #region Variables
+        private static int w = 12;
+        private static int h = 15;
+
+        public static CellState[,] mineField = new CellState[h, w];
+
+        #endregion
+        #region
+        public static void Init()
+        {
+            for (int i = 0; i < h; i++)
+            {
+                for (int j = 0; j < w; j++)
+                {
+                    mineField[i, j] = CellState.Empty;
+                }
+            }
+        }
+
+        #endregion
+    }
+}
--- WindowsFormsApp2/Views/FormMain.cs
+++ WindowsFormsApp2/Views/FormMain.cs
@@ -8,6 +8,8 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using WindowsFormsApp2.Views;
+using WindowsFormsApp2.Controller;
+using WindowsFormsApp2.Model;
 
 namespace WindowsFormsApp2
 {
@@ -27,11 +29,15 @@ namespace WindowsFormsApp2
         {
             InitializeComponent();
             PrepairField();
+            Game.Init();
 
 
 
         }
-
+        /// <summary>
+        /// Добавление на рабочее поле ячеек
+        /// </summary>
+        /// 
         private void PrepairField()
         {
             for (int j = 0; j < 15; j++)
@@ -44,8 +50,7 @@ namespace WindowsFormsApp2
                         Left = size + i * size,
                         Top = size + j * size,
                         Width = size,
-                        Height = size,
-                        
+                        Height = size,                        
                     };
                     
                     cell.Click += new EventHandler(cell_Click);
@@ -56,14 +61,34 @@ namespace WindowsFormsApp2
         
         #region Event
 
+        /// <summary>
+        /// Обработчик нажатия ячеек
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void cell_Click(object sender, System.EventArgs e)
         {
-             string massege = string.Format("i:{0}, i:{1}, i:({2})",
-                 (sender as Cell).i,
-                 (sender as Cell).j,
-                 (sender as Cell).Parent.Name);
-             MessageBox.Show(massege);
-        }
+             //string massege = string.Format("i:{0}, j:{1}, i:({2})",
+             //    (sender as Cell).i,
+             //    (sender as Cell).j,
+             //    (sender as Cell).Parent.Name);
+             //MessageBox.Show(massege);
+            switch (Game.mineField[(sender as Cell).i, (sender as Cell).j])
+            {
+                case CellState.Empty:
+                    Game.mineField[(sender as Cell).i, (sender as Cell).j] = CellState.Flag;
+                    break;
+                case CellState.Flag:
+                    Game.mineField[(sender as Cell).i, (sender as Cell).j] = CellState.Question;
+                   // cell((sender as Cell).i, (sender as Cell).j).name = "P";
+                    break;
+                case CellState.Question:
+                    Game.mineField[(sender as Cell).i, (sender as Cell).j] = CellState.Empty;
+                    break;
+            }
+              MessageBox.Show(Game.mineField[(sender as Cell).i, (sender as Cell).j].ToString());
+
+        }        
         #endregion
 
     }
